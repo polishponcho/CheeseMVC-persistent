8cf2882a940aef74e7e22a920d676baf727c462c
Ryan Ratajczyk <polishponcho@gmail.com>
2018-12-18 17:53:06 -0600

    this is everything

---
+++ Migrations/20181218005216_AddCategory.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CheeseMVC.Migrations
+{
+    public partial class AddCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Cheeses",
+                newName: "CategoryID");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: true),
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menu",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: true),
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menu", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CheeseMenu",
+                columns: table => new
+                {
+                    MenuID = table.Column<int>(nullable: false),
+                    CheeseID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheeseMenu", x => new { x.CheeseID, x.MenuID });
+                    table.ForeignKey(
+                        name: "FK_CheeseMenu_Cheeses_CheeseID",
+                        column: x => x.CheeseID,
+                        principalTable: "Cheeses",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CheeseMenu_Menu_MenuID",
+                        column: x => x.MenuID,
+                        principalTable: "Menu",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cheeses_CategoryID",
+                table: "Cheeses",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CheeseMenu_MenuID",
+                table: "CheeseMenu",
+                column: "MenuID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cheeses_Categories_CategoryID",
+                table: "Cheeses",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cheeses_Categories_CategoryID",
+                table: "Cheeses");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "CheeseMenu");
+
+            migrationBuilder.DropTable(
+                name: "Menu");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cheeses_CategoryID",
+                table: "Cheeses");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryID",
+                table: "Cheeses",
+                newName: "Type");
+        }
+    }
+}
---
+++ Migrations/20181218223528_AddMenu.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CheeseMVC.Migrations
+{
+    public partial class AddMenu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CheeseMenu_Cheeses_CheeseID",
+                table: "CheeseMenu");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CheeseMenu_Menu_MenuID",
+                table: "CheeseMenu");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Menu",
+                table: "Menu");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CheeseMenu",
+                table: "CheeseMenu");
+
+            migrationBuilder.RenameTable(
+                name: "Menu",
+                newName: "Menus");
+
+            migrationBuilder.RenameTable(
+                name: "CheeseMenu",
+                newName: "CheeseMenus");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CheeseMenu_MenuID",
+                table: "CheeseMenus",
+                newName: "IX_CheeseMenus_MenuID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Menus",
+                table: "Menus",
+                column: "ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CheeseMenus",
+                table: "CheeseMenus",
+                columns: new[] { "CheeseID", "MenuID" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CheeseMenus_Cheeses_CheeseID",
+                table: "CheeseMenus",
+                column: "CheeseID",
+                principalTable: "Cheeses",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CheeseMenus_Menus_MenuID",
+                table: "CheeseMenus",
+                column: "MenuID",
+                principalTable: "Menus",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CheeseMenus_Cheeses_CheeseID",
+                table: "CheeseMenus");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CheeseMenus_Menus_MenuID",
+                table: "CheeseMenus");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Menus",
+                table: "Menus");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CheeseMenus",
+                table: "CheeseMenus");
+
+            migrationBuilder.RenameTable(
+                name: "Menus",
+                newName: "Menu");
+
+            migrationBuilder.RenameTable(
+                name: "CheeseMenus",
+                newName: "CheeseMenu");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CheeseMenus_MenuID",
+                table: "CheeseMenu",
+                newName: "IX_CheeseMenu_MenuID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Menu",
+                table: "Menu",
+                column: "ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CheeseMenu",
+                table: "CheeseMenu",
+                columns: new[] { "CheeseID", "MenuID" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CheeseMenu_Cheeses_CheeseID",
+                table: "CheeseMenu",
+                column: "CheeseID",
+                principalTable: "Cheeses",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CheeseMenu_Menu_MenuID",
+                table: "CheeseMenu",
+                column: "MenuID",
+                principalTable: "Menu",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
